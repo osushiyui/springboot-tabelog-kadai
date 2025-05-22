8962ac474392c230dd34c4279b6daafd264d833b
osushiyui <ky.ks.fh.04@gmail.com>
2025-05-23 06:57:50 +0900

    Delete static directory

--- static/js/flatpickr.js
+++
@@ -1,8 +0,0 @@
-let maxDate = new Date();
-maxDate = maxDate.setMonth(maxDate.getMonth() + 3);
-
-flatpickr('#fromCheckinDateToCheckoutDate', {
- locale: 'ja',
- minDate: 'today',
- maxDate: maxDate
-});
