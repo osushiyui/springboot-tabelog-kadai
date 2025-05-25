ea0e63525004f90ca73f39d7ee34fc723640a1ad
osushiyui <ky.ks.fh.04@gmail.com>
2025-05-25 20:37:56 +0900

    Delete resources directory

--- resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51QuYTm4CcUmRLRc6SAMz5cQwDkRyIK8Yrma5JNPAaIuHvvJUqL595obZV0ydubW7W3p5TE0ObFRQrEj9h00CsnlV00XwZvbbBV');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
